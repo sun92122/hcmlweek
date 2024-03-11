57417ead3aa079f89ffdcc3b967047d3ec2e5488
magical <sun920122@gmail.com>
2024-03-11 22:09:35 +0800

    💄 Update font-family in app.vue

--- app.vue
+++ app.vue
@@ -8,8 +8,11 @@
 </template>
 
 <style>
+@import url(//fonts.googleapis.com/earlyaccess/notosanstc.css);
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Noto Sans TC", "Helvetica", "Arial", "LiHei Pro", "黑體-繁",
+    "微軟正黑體", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
