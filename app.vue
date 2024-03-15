30af003eacb847a648e1d658147866a77257b806
magical <sun920122@gmail.com>
2024-03-16 03:04:24 +0800

    🔍 Remove Cloudflare Web Analytics and add nuxt-cloudflare-analytics plugin

--- app.vue
+++ app.vue
@@ -1,11 +1,4 @@
 <template>
-  <!-- Cloudflare Web Analytics -->
-  <script
-    defer
-    src="https://static.cloudflareinsights.com/beacon.min.js"
-    data-cf-beacon='{"token": "b09109352b614b4892eaaa859337bd12"}'
-  ></script>
-  <!-- End Cloudflare Web Analytics -->
   <div id="app">
     <NuxtLoadingIndicator />
     <NuxtLayout />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,13 @@ export default defineNuxtConfig({
     "@pinia/nuxt",
     "@pinia-plugin-persistedstate/nuxt",
     "@nuxtjs/google-fonts",
+    "nuxt-cloudflare-analytics",
   ],
+
+  cloudflareAnalytics: {
+    // See below for more options
+    token: "b09109352b614b4892eaaa859337bd12", // Example 1a2b3v4a5er6ac7r8afd
+  },
 
   pinia: {
     storesDirs: ["~/store/**"],
