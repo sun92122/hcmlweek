9732bae5f7f1b68871a024fad76ea03b806c8ef4
magical <sun920122@gmail.com>
2024-03-18 02:46:31 +0800

    ✨ Add help page content and styles

--- pages/help/index.vue
+++ pages/help/index.vue
@@ -1,1 +1,116 @@
-<template></template>
+<template>
+  <!-- 
+HELP
+
+🌱預購商品付款方式：
+於正式週3/24-4/12期間，至竹苗週攤位付款（現金／LINE Pay）
+
+🌱預購商品取貨地點及時間：
+1. 熱食預購商品：
+日期：
+西大發：3/25-4/12
+晌午粄食、彭老師野薑花粽、老街一口菜包：4/8-4/12
+貢丸湯、酒釀湯圓、仙草凍：3/25、3/27、3/29
+時間：12:30-13:00
+地點：竹苗週攤位
+
+2.非熱食預購商品：
+日期：4/18、4/19 
+時間：12:30-13:00 
+地點：誠正中庭
+
+▪︎若非熱食預購商品的領取時間不克前來，請將您方便的時間填在訂單備註💖
+
+🤑預購抽獎資格：
+於3/18-3/22下單滿60元即擁有抽獎資格！
+
+🤑預購抽獎獎品：
+1. 豪哥炸雞 10元折價券×10
+2. 燈籠滷味創始店 10元折價券×10
+3. 茶壜 20元折價券×20
+
+（3/25會公布中獎名單於竹苗週IG帳號，折價券於取貨時一併領取）
+
+🌱與竹苗的聯絡方式：
+有任何其他疑問都可以私訊IG帳號：@hcmlweek
+-->
+  <div class="help-container font-sans">
+    <div class="help-title-container">
+      <span class="help-title">竹苗文化週</span>
+      <span class="help-subtitle">2024 師大竹苗文化週</span>
+    </div>
+    <div class="help-paragraph">
+      <span id="payment">🌱預購商品付款方式：</span>
+      <span>　　於正式週3/24-4/12期間，至竹苗週攤位付款（現金／LINE Pay）</span>
+    </div>
+    <div class="help-paragraph">
+      <span>🌱預購商品取貨地點及時間：</span>
+      <span>　1. 熱食預購商品：</span>
+      <span>　　日期：</span>
+      <span>　　西大發：3/25-4/12</span>
+      <span>　　晌午粄食、彭老師野薑花粽、老街一口菜包：4/8-4/12</span>
+      <span>　　貢丸湯、酒釀湯圓、仙草凍：3/25、3/27、3/29</span>
+      <span>　　時間：12:30-13:00</span>
+      <span>　　地點：竹苗週攤位</span>
+      <span>　</span>
+      <span>　2. 非熱食預購商品：</span>
+      <span>　　日期：4/18、4/19</span>
+      <span>　　時間：12:30-13:00</span>
+      <span>　　地點：誠正中庭</span>
+      <span>　</span>
+      <span
+        >▪若非熱食預購商品的領取時間不克前來，請將您方便的時間填在訂單備註💖</span
+      >
+    </div>
+    <div class="help-paragraph">
+      <span id="lottery">🤑預購抽獎資格：</span>
+      <span>　　於3/18-3/22下單滿60元即擁有抽獎資格！</span>
+      <span>　</span>
+      <span>🤑預購抽獎獎品：</span>
+      <span>　1. 豪哥炸雞 10元折價券×10</span>
+      <span>　2. 燈籠滷味創始店 10元折價券×10</span>
+      <span>　3. 茶壜 20元折價券×20</span>
+      <span>　</span>
+      <span>（3/25會公布中獎名單於竹苗週IG帳號，折價券於取貨時一併領取）</span>
+    </div>
+    <div class="help-paragraph">
+      <span>🌱與竹苗的聯絡方式：</span>
+      <span
+        >　　有任何其他疑問都可以私訊IG帳號：<ULink
+          to="https://www.instagram.com/hcmlweek"
+          target="_blank"
+          ><u>@hcmlweek</u></ULink
+        ></span
+      >
+    </div>
+  </div>
+</template>
+
+<style>
+.help-container {
+  width: 550px;
+  max-width: 90vw;
+  margin: auto;
+  text-align: left;
+}
+
+.help-title-container {
+  text-align: center;
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+}
+
+.help-subtitle {
+  font-size: 1.5rem;
+  font-weight: normal;
+}
+
+.help-paragraph {
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+}
+</style>
